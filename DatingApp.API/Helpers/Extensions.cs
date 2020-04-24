ed4c5bdca1bbaaa466bb6bcd0c7383a041bd7994
akshay m <aekkim.13@gmail.com>
2020-04-24 18:10:12 +0530

    Module 14 A - Pagination

--- DatingApp.API/Helpers/Extensions.cs
+++ DatingApp.API/Helpers/Extensions.cs
@@ -1,5 +1,7 @@
 using System;
 using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace DatingApp.API.Helpers
 {
@@ -10,6 +12,16 @@ namespace DatingApp.API.Helpers
             res.Headers.Add("Application-Error", message);
             res.Headers.Add("Access-Control-Expose-Headers", "Application-Error");
             res.Headers.Add("Access-Control-Allow-Origin", "*");
+        }
+
+        public static void AddPaginationHeader(this HttpResponse response, int currentPage,
+                int itemsPerPage, int totalPages, int totalItems)
+        {
+            var pageObject = new PaginationHeader(currentPage, itemsPerPage, totalPages, totalItems);
+            var camelCaseFormatter = new JsonSerializerSettings();
+            camelCaseFormatter.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            response.Headers.Add("Pagination", JsonConvert.SerializeObject(pageObject, camelCaseFormatter));
+            response.Headers.Add("Access-Control-Expose-Headers", "Pagination");
         }
 
         public static int CalculateAge(this DateTime dateObj)
